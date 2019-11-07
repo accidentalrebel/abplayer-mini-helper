611e7d814803152e145cedd2f860b94955e4c4fd
accidentalrebel <accidentalrebel@gmail.com>
2019-11-07 21:42:54 +0800

    Can now cut to an output directory

--- abplayer.py
+++ abplayer.py
@@ -1,5 +1,8 @@
+#!/usr/bin/env python3
 import subprocess
 import argparse
+import os
+import sys
 
 parser = argparse.ArgumentParser()
 parser.add_argument('-c',
@@ -11,7 +14,9 @@ parser.add_argument('-c',
 args = parser.parse_args()
 
 if args.cut:
-    source = args.cut[0]
+    source = os.path.expanduser(os.path.abspath(args.cut[0]))
+    print(source)
+
     duration = args.cut[1]
     if not "." in duration:
         raise ValueError("Duration was not in decimal form. Received " + duration)
@@ -20,12 +25,19 @@ if args.cut:
     splitted = source.split('/')
     
     directory = splitted[1:len(splitted)-1]
-    directory = '/'.join(directory)
+    directory = '/' + '/'.join(directory)
+    print(directory)
     
     filename = splitted[len(splitted)-1]
     filename = filename.replace('.mp3', '')
-    
-    cmd = "sox -V3 " + args.cut[0] + " " + directory + "/" + filename + "_part_.mp3 silence -l 0 1 " + duration + " 0.1% : newfile : restart"
+
+    destination = directory + "/output/"
+    print(destination)
+
+    if not os.path.isdir(destination):
+        os.mkdir(destination)
+
+    cmd = "sox -V3 " + source + " " + destination + ".mp3 silence -l 0 1 " + duration + " 0.1% : newfile : restart"
     print(cmd)
     # subprocess.call(cmd, shell=True)
 
