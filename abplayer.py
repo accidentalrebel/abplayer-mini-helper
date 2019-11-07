e51b3ea75efc9ef33e207a9fbe5f9da4f15e106c
accidentalrebel <accidentalrebel@gmail.com>
2019-11-07 21:50:25 +0800

    Can now count files generated

--- abplayer.py
+++ abplayer.py
@@ -7,38 +7,34 @@ import sys
 parser = argparse.ArgumentParser()
 parser.add_argument('-c',
                     '--cut',
-                    nargs=2,
+                    nargs=3,
                     help='Cuts the file specified in SOURCE with DURATION in seconds (e.g. 10.0)',
-                    metavar=('SOURCE','DURATION'))
+                    metavar=('SOURCE','DESTINATION','DURATION'))
 
 args = parser.parse_args()
 
 if args.cut:
     source = os.path.expanduser(os.path.abspath(args.cut[0]))
-    print(source)
-
-    duration = args.cut[1]
+    destination = args.cut[1]
+    duration = args.cut[2]
+    
     if not "." in duration:
         raise ValueError("Duration was not in decimal form. Received " + duration)
         sys.exit()
     
     splitted = source.split('/')
     
-    directory = splitted[1:len(splitted)-1]
-    directory = '/' + '/'.join(directory)
-    print(directory)
-    
     filename = splitted[len(splitted)-1]
     filename = filename.replace('.mp3', '')
-
-    destination = directory + "/output/"
-    print(destination)
 
     if not os.path.isdir(destination):
         os.mkdir(destination)
 
-    cmd = "sox -V3 " + source + " " + destination + ".mp3 silence -l 0 1 " + duration + " 0.1% : newfile : restart"
+    cmd = "sox -V3 \"" + source + "\" \"" + destination + ".mp3\" silence -l 0 1 " + duration + " 0.1% : newfile : restart"
     print(cmd)
-    # subprocess.call(cmd, shell=True)
+    #subprocess.call(cmd, shell=True)
+
+    count = subprocess.check_output("ls -1 \"" + destination + "\" | wc -l", shell=True, encoding='UTF-8')
+    print("Files generated: " + str(count))
 
 
